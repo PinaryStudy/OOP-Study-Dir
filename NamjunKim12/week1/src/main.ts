601a56bda3ff0a3f30ef2a3766da18b34f7aa415
1 <wrasf175@naver.com>
2023-06-04 17:22:28 +0900

    style: html/css 기본 마크업 구조, 스타일 세팅

--- NamjunKim12/week1/src/main.ts
+++
@@ -1,8 +0,0 @@
-import './style.css'
-import { setupCounter } from './counter.ts'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1>Hello Vite!</h1>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
--- NamjunKim12/week1/tests/Dollar.test.js
+++
@@ -1,9 +0,0 @@
-import {Dollar} from "../src/Dollar";
-
-test('곱셈 테스트', () => {
-    const dollar = new Dollar(5);
-
-    dollar.times(2);
-
-    expect(dollar.amount).toBe(10);
-});
