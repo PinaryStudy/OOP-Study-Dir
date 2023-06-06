6b69e10ce1cd5fe6575d74ee5038ccecf702ade0
yungu0010 <dbstj16@sookmyung.ac.kr>
2023-06-07 01:01:16 +0900

    Calculator 인터페이스 구현 OperationType에 따라 Utils의 메소드 호출

--- yungu0010/OOP-Study/OOP-Study/Calculator.swift
+++ yungu0010/OOP-Study/OOP-Study/Calculator.swift
@@ -1,5 +1,5 @@
 //
-//  Calculator.swift
+//  Caculator.swift
 //  OOP-Study
 //
 //  Created by 강윤서 on 2023/06/07.
@@ -7,10 +7,36 @@
 
 import Foundation
 
+enum OperationType: String {
+    case plus = "+"
+    case minus = "-"
+    case multiple = "*"
+    case divide = "/"
+}
+
 class Calculator {
     private var utils = Utils()
     
     public func calculate(operation: [String], numbers: [Float]) -> Float {
-        return 0
+        var left = numbers[0]
+        
+        for index in 0...operation.count-1 {
+            let op = operation[index]
+            switch op {
+            case OperationType.plus.rawValue:
+                left = utils.plus(firstNumber: left,
+                                  secondNumber: numbers[index+1])
+            case OperationType.minus.rawValue:
+                left = utils.minus(firstNumber: left,
+                                   secondNumber: numbers[index+1])
+            case OperationType.multiple.rawValue:
+                left = utils.multiple(firstNumber: left,
+                                      secondNumber: numbers[index+1])
+            default:
+                left = utils.divide(firstNumber: left,
+                                    secondNumber: numbers[index+1])
+            }
+        }
+        return left
     }
 }
